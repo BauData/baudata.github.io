4481e982ace557a0fc61bccb69ddd594bceb91df
BauData <baudata@protonmail.com>
2017-03-16 02:41:45 +0100

    adding forms

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
-var socket;
+// Get the modal
+var modalJoin = document.getElementById('modalJoin');
+var modalSubmit = document.getElementById('modalSubmit');
+// Get the button that opens the modal
+var joinUs = document.getElementById("buttonJoin");
+var submitTopic = document.getElementById("buttonSubmitTopic");
+// When the user clicks on the button, open the modal 
+joinUs.onclick = function() {
+    modalJoin.style.display = "block";
+}
+submitTopic.onclick = function() {
+    modalSubmit.style.display = "block";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modalJoin) {
+        modalJoin.style.display = "none";
+    }
+    else if (event.target == modalSubmit) {
+        modalSubmit.style.display = "none";
+    }
+}
