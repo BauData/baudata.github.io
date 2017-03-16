efd5d4d47cf663d0cc18c0ce50a0bb1275d7adbe
BauData <baudata@protonmail.com>
2017-03-16 12:21:45 +0100

    adding email pattern and required attribute

--- gruntfile.js
+++ gruntfile.js
@@ -8,12 +8,6 @@ module.exports = function(grunt) {
       'dist/js/scripts.min.js': ['dist/js/scripts.min.js']
     },
     copy: {
-      /*index: {
-        expand: true,
-        cwd: 'src/',
-        src: ['index.html'],
-        dest: 'dist/'
-      },*/
       css: {
         expand: true,
         cwd: 'src/css/',
@@ -38,19 +32,12 @@ module.exports = function(grunt) {
               ]
           }
       }
-    },
-   /* 'gh-pages': {
-    options: {
-      base: 'dist'
-    },
-    src: ['**']
-  }*/
+    }
   });
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-browserify');
-  //grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('default', '', function() {
     var taskList = [
